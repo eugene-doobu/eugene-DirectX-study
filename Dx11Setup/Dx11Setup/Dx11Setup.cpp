e18c4b05e833287597020941cf5ca4cff33f241c
eugene-doobu <ssundong0_0@naver.com>
2021-07-20 14:53:54 +0900

    Dx11Setup

---
+++ Dx11Setup/Dx11Setup/Dx11Setup.cpp
@@ -0,0 +1,251 @@
+﻿// Dx11Setup.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+//
+#include "framework.h"
+#include "Dx11Setup.h"
+
+#include <d3d11.h>
+#include <assert.h>
+
+#define MAX_LOADSTRING 100
+
+// 전역 변수:
+HINSTANCE hInst;                                // 현재 인스턴스입니다.
+WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
+WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+
+                                                
+// For initialization and utility
+ID3D11Device* g_Device;
+IDXGISwapChain* g_Swapchain;
+ID3D11DeviceContext* g_DeviceContext;
+float g_aspectRatio;
+
+// For drawing  -> New stuff right here
+ID3D11RenderTargetView* g_RenderTargetView;
+D3D11_VIEWPORT g_viewport;
+
+// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_DX11SETUP, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_DX11SETUP));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (true)
+    {
+        // GetMessage로 루프 시작 전에 메시지를 기다리는 경우 그래픽 애플리케이션에서 문제 발생
+        PeekMessage(&msg, NULL, 0, 0, PM_REMOVE);
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+
+        if (msg.message == WM_QUIT)
+            break;
+
+        // Break if user presses escape key
+        if (GetAsyncKeyState(VK_ESCAPE)) break;
+
+        // Rendering Code
+        ID3D11RenderTargetView* tempRTV[] = { g_RenderTargetView };
+        g_DeviceContext->OMSetRenderTargets(1, tempRTV, nullptr);
+
+        float color[4] = { 1, 0, 0, 1 };
+        g_DeviceContext->ClearRenderTargetView(g_RenderTargetView, color);
+        g_DeviceContext->RSSetViewports(1, &g_viewport); // 배열이라 속이기 위해 포인터이용. 추천하지 않는 방법
+
+        g_Swapchain->Present(0, 0);
+    }
+
+    // ~
+    g_Device->Release();
+    g_DeviceContext->Release();
+    g_RenderTargetView->Release();
+    g_Swapchain->Release();
+
+    return (int) msg.wParam;
+}
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_DX11SETUP));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_DX11SETUP);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   // 스왑 체인, 장치, 장치 컨텍스트 설정
+   // D3d11 code here
+   RECT rect;
+   GetClientRect(hWnd, &rect);
+
+   // Attach d3d to the window
+   D3D_FEATURE_LEVEL DX11 = D3D_FEATURE_LEVEL_11_0;
+   DXGI_SWAP_CHAIN_DESC swap;
+   ZeroMemory(&swap, sizeof(DXGI_SWAP_CHAIN_DESC));
+   swap.BufferCount = 1;
+   swap.OutputWindow = hWnd;
+   swap.Windowed = true;
+   swap.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+   swap.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+   swap.BufferDesc.Width = rect.right - rect.left;
+   swap.BufferDesc.Height = rect.bottom - rect.top;
+   swap.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+   swap.SampleDesc.Count = 1;
+
+   g_aspectRatio = swap.BufferDesc.Width / (float)swap.BufferDesc.Height;
+
+   HRESULT result;
+
+   result = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, NULL, D3D11_CREATE_DEVICE_DEBUG, &DX11, 1, D3D11_SDK_VERSION, &swap, &g_Swapchain, &g_Device, 0, &g_DeviceContext);
+   assert(!FAILED(result));
+
+   // 백 버퍼/뷰포트 설정
+   ID3D11Resource* backbuffer;
+   result = g_Swapchain->GetBuffer(0, __uuidof(backbuffer), (void**)&backbuffer);
+   result = g_Device->CreateRenderTargetView(backbuffer, NULL, &g_RenderTargetView);
+   assert(!FAILED(result));
+
+   // Release the resource to decrement the counter by one
+   // This is necessary to keep the buffer from leaking memory
+   backbuffer->Release();
+
+   // Setup viewport
+   g_viewport.Width = swap.BufferDesc.Width;
+   g_viewport.Height = swap.BufferDesc.Height;
+   g_viewport.TopLeftY = g_viewport.TopLeftX = 0;
+   g_viewport.MinDepth = 0;
+   g_viewport.MaxDepth = 1;
+
+   // D3d11 code here
+
+   return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // 메뉴 선택을 구문 분석합니다:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
