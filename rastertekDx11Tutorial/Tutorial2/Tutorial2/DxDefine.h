c16bb86120cba91818850c56783213aef6d45fee
eugene-doobu <58686228+eugene-doobu@users.noreply.github.com>
2021-07-21 22:28:29 +0900

    rastertek 3.2
    rastertek 3강 조금 했음

--- rastertekDx11Tutorial/Tutorial2/Tutorial2/DxDefine.h
+++ rastertekDx11Tutorial/Tutorial2/Tutorial2/DxDefine.h
@@ -1,1 +1,15 @@
 #pragma once
+
+// LINKING
+
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+// INCLUDES
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+#include "AlignedAllocationPolicy.h"
--- rastertekDx11Tutorial/Tutorial2/Tutorial2/GraphicsClass.h
+++ rastertekDx11Tutorial/Tutorial2/Tutorial2/GraphicsClass.h
@@ -5,6 +5,8 @@ const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
 
+class D3DClass;
+
 class GraphicsClass
 {
 public:
@@ -18,4 +20,7 @@ public:
 
 private:
 	bool Render();
+
+private:
+	D3DClass* m_Direct3D = nullptr;
 };
